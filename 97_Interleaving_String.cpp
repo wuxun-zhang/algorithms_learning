405524b146633cad2aab18e35ca94945bb04f5d2
zwx962464 <34727741+zwx962464@users.noreply.github.com>
2018-06-07 09:35:10 +0800

    string/dynamic programming

--- 97_Interleaving_String.cpp
+++ 97_Interleaving_String.cpp
@@ -1,3 +1,64 @@
 #include<iostream>
+#include<vector>
 #include<string>
 using namespace std;
+
+//Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2. 
+// 2-D dp
+bool isInterleave_v1(const string& s1, const string &s2, const string& s3)
+{
+	int l1 = s1.size(), l2 = s2.size(), l3 = s3.size();
+	if (l3 != l1 + l2)
+		return false;
+	vector<vector<bool>> dp(l1 + 1, vector<bool>(l2 + 1));
+	for (int i = 0; i <= l1; i++)
+	{
+		for (int j = 0; j <= l2; j++)
+		{
+			if (i == 0 && j == 0)
+				dp[i][j] = true;
+			else if (i == 0)
+				dp[i][j] = dp[i][j - 1] && (s2[j - 1] == s3[i + j - 1]);
+			else if (j == 0)
+				dp[i][j] = dp[i - 1][j] && (s1[i - 1] == s3[i + j - 1]);
+			else {
+				dp[i][j] = ((dp[i - 1][j] && (s1[i - 1] == s3[i + j - 1])) || (dp[i][j - 1] && (s2[j - 1] == s3[i + j - 1])));
+			}
+		}
+	}
+	return dp[l1][l2];
+}
+
+// 1-D dp
+bool isInterleave_v2(const string& s1, const string &s2, const string& s3)
+{
+	int l1 = s1.size(), l2 = s2.size(), l3 = s3.size();
+	if (l3 != l1 + l2)
+		return false;
+	vector<bool> dp(l2 + 1);
+	for (int i = 0; i <= l1; i++)
+	{
+		for (int j = 0; j <= l2; j++)
+		{
+			if (i == 0 && j == 0)
+				dp[j] = true;
+			else if (i == 0)
+				dp[j] = dp[j - 1] && (s2[j - 1] == s3[i + j - 1]);
+			else if (j == 0)
+				dp[j] = dp[j] && (s1[i - 1] == s3[i + j - 1]);
+			else {
+				dp[j] = ((dp[j] && (s1[i - 1] == s3[i + j - 1])) || (dp[j - 1] && (s2[j - 1] == s3[i + j - 1])));
+			}
+		}
+	}
+	return dp[l2];
+}
+
+void main()
+{
+	string s1, s2, s3;
+	while (cin >> s1 >> s2 >> s3)
+	{
+		cout << (isInterleave_v2(s1, s2, s3) ? "Yes":"No") << endl;
+	}
+}
